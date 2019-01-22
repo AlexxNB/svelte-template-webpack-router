5f6008b3ea9a0e898be82f4ac5733bccd218294d
Alexey Schebelev <iam@alexxnb.ru>
2019-01-22 23:34:14 +0300

    refact: scss preprocessor same as in webpak config

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,9 @@
 const sass = require('node-sass');
 
 module.exports = {
-    preprocess: {
-        style: async ({ content, attributes }) => {
-            if (attributes.type !== 'text/scss') return;
-
-            return new Promise((resolve, reject) => {
-                sass.render(
-                    {
-                        data: content,
-                        includePaths: [],
-                        sourceMap: true,
-                        outFile: 'x', // this is necessary, but is ignored
-                    },
-                    (err, result) => {
-                        if (err) return reject(err);
-
-                        resolve({
-                            code: result.css.toString(),
-                            map: result.map.toString(),
-                        });
-                    },
-                );
-            });
-        },
-    },
+    preprocess: require('svelte-preprocess')({ 
+        transformers: {
+            scss: true,
+        }
+    })
 };
